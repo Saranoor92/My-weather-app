254ec2c68404e9edc7991a317a713006ae8a9414
Saranoor92 <saranoor1992@gmail.com>
2023-07-13 05:01:52 -0400

    trying to fix forecast

--- src/index.js
+++ src/index.js
@@ -41,28 +41,25 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-            <div class="col-2">
-                <div c  lass = "weather-forecast-date">${formatDay(
-                  forecastDay.time * 1000
-                )} </div>
-            
-                <img src="${forecastDay.condition.icon_url}"  
-                alt=""  
-                width="42"
-                />
-                <div class="weather-forecast-temperatures">
-                    <span class=
-                    "weather-forecast-temperature-max">
-                         ${Math.round(forecastDay.temperature.max)}° 
-                         </span>
-
-                         <span class= 
-                         "weather-forecast-temperature-min">
-                        ${Math.round(forecastDay.temperature.min)}°
-                            </span>
-                          </div>
-                          </div>
-                          `;
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
     }
   });
 
